b0d94d71cfa47a37bf29c4cc6b1df604ea6ea3f7
ofir <ofir@lupa.co.il>
2024-07-30 10:07:21 +0300

    New pull R

--- Project_form/Page_form/page2.py
+++ Project_form/Page_form/page2.py
@@ -1,5 +1,3 @@
-import time
-
 from Project_form.conftest import *
 
 
--- Project_form/test_form/test_page2.py
+++ Project_form/test_form/test_page2.py
@@ -5,6 +5,6 @@ from Project_form.Page_form.page2 import *
 
 
 @pytest.mark.group2
-def test_2():
-    s2 = Page2()
+def test_2(driver):
+    s2 = Page2(driver)
     s2.fills(5,"כחול")
