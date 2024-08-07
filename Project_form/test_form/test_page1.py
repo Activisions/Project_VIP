2abb69e475dc765a436fb7f36d7cc563906b1432
ofir <ofir@lupa.co.il>
2024-08-07 20:43:03 +0300

    New pull R

--- Project_form/test_form/test_page1.py
+++ Project_form/test_form/test_page1.py
@@ -7,5 +7,5 @@ def test_1(driver) :
     s1 = Page1(driver)
     s1.fill("daniel","levy","0501234567","a@a.com")
     f_name, l_name, phone_number, email, radio, btn_step2 = s1.locators()
-    assert radio.get_attribute("value") == "דרום"
+    assert radio.get_attribute("value") == "דרום1"
 
