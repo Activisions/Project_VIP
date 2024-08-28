5436cfebd7a11774b864fd6798f808cc0709108d
ofir <ofir@lupa.co.il>
2024-08-28 15:24:21 +0300

    New pull R

--- Project_HackerU/API/request_test.py
+++ Project_HackerU/API/request_test.py
@@ -3,7 +3,8 @@ import json
 
 
 
-#לא להתייחס נחליף בהמשך
+#לא להתייחס נחליף פרטים ואתר בהמשך בהתאם לפרוייקט שעליו נעבוד
+
 def test_get_posts_status_code():
     response = requests.get("http://localhost:3000/posts")
     assert response.status_code == 200
