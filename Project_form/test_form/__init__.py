e5ba8cab131e4eb91bece91b3d874f456a6d4acf
ofir <ofir@lupa.co.il>
2024-08-30 00:46:53 +0300

    New pull R

--- Project_form/test_form/__init__.py
+++
@@ -1,6 +0,0 @@
-#                       @pytest.mark.skip()                                              דילוג טסט
-#                       @pytest.mark.dependency()                            תלויות מגדירים בראשון
-#                       @pytest.mark.dependency(depends=["test_"])                    מגדירים בשני
-#                       @pytest.mark.flaky(reruns=3)                              ניסיוניות חוזרות
-
-
--- Project_form/test_form/test_page2.py
+++
@@ -1,7 +0,0 @@
-from Project_form.Page_form.page2 import *
-
-
-@pytest.mark.group2
-def test_2(driver):
-    s2 = Page2(driver)
-    s2.fills(5,"כחול")
