cc00f0085f0e99268683c0aa35f823d935de3e0d
ofir <ofir@lupa.co.il>
2024-08-29 15:55:25 +0300

    New pull R

--- Project_HackerU/A_SITE/page_site/menu.py
+++ Project_HackerU/A_SITE/page_site/menu.py
@@ -3,6 +3,6 @@ from imports import *
 
 
 
-def test_ramilevi(driver):
-    driver.get("https://israelpost.co.il/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D/%D7%A6%D7%95%D7%A8-%D7%A7%D7%A9%D7%A8/")
+def test_sdf(driver):
+    driver.get("https://www.danhotels.co.il/")
     hold()
