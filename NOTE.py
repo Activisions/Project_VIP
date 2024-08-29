8dd34168cdb408708cf27ccee6181135f12b8360
ofir <ofir@lupa.co.il>
2024-08-29 20:19:12 +0300

    New pull R

--- NOTE.py
+++ NOTE.py
@@ -5,3 +5,4 @@ def test_sdf3(driver):
     driver.get("https://www.danhotels.co.il/eDan/Registration")
     driver.find_element(By.XPATH,"(//span[contains(@class,'MuiCheckbox-root MuiCheckbox-colorPrimary')])[2]").click()
     hold()
+1
