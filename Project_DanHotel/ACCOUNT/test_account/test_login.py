f98e5f5227bca59aeab886f57d5e599d1b6aeee2
ofir <ofir@lupa.co.il>
2024-08-31 21:02:39 +0300

    New pull R

--- Project_DanHotel/ACCOUNT/test_account/test_login.py
+++ Project_DanHotel/ACCOUNT/test_account/test_login.py
@@ -6,6 +6,8 @@ from Project_DanHotel.ACCOUNT.page_account.login import *
 
 login_url = "https://www.danhotels.co.il/eDan/Login"
 
+#פונקציית טסט שמבצעת לוג אין בהצלחה
+
 def test_login(driver):
     driver.get(login_url)
     connector = Login(driver)
