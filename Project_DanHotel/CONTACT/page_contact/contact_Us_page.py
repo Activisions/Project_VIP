d15ff5d867415f0dfd8ec30153c7bf4eaef8aeaf
galsh402 <134851412+galsh402@users.noreply.github.com>
2024-08-31 22:32:15 +0300

    הוספתי אחי עמוד של יצירת קשר עם 6 צעדי בדיקה
    בנוסף מצרף לך את מה שדיברנו על העמודים של החיפוש חדר בניתי שם את הפונקציה אבל היא לא תעבוד.
    
    עדכן אותי מה הצלחת במידה ואתה מצליח שם (:

---
+++ Project_DanHotel/CONTACT/page_contact/contact_Us_page.py
@@ -0,0 +1,67 @@
+from imports import *
+
+
+class contact_Us:
+    def __init__(self, driver):
+        self.driver: WebDriver = driver
+
+    def contact_locators(self):
+        contact_name = self.driver.find_element(By.XPATH, "//*[@id=\"edit-name\"]")
+        contact_email = self.driver.find_element(By.XPATH, "//*[@id=\"edit-email\"]")
+        contact_phone = self.driver.find_element(By.XPATH, "//*[@id=\"edit-phone\"]")
+        choose_hotel_dropdown = self.driver.find_element(By.NAME, "hotel_name")
+        choose_hotel = self.driver.find_element(By.XPATH, "//*[@id=\"edit-hotel-name\"]/option[3]")
+        choose_subject_dropdown = self.driver.find_element(By.ID, "edit-subject")
+        choose_subject = self.driver.find_element(By.XPATH, "//*[@id=\"edit-subject\"]/option[5]")
+        contact_note = self.driver.find_element(By.ID, "edit-message")
+        contact_button = self.driver.find_element(By.ID, "edit-actions-submit")
+        return contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button
+
+    #פונקציה למילוי פרטים (טלפון ,אימייל ,טלפון)
+    def contact_us_fill(self, name, email, phone, nots):
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button = self.contact_locators()
+        contact_name.send_keys(name)
+        contact_email.send_keys(email)
+        contact_phone.send_keys(phone)
+        contact_note.send_keys(nots)
+
+        try:
+            contact_button.click()
+        except Exception as e:
+            print("No contact:")
+
+        #פונקציה למילוי פרטים (עם בחירת מלון בלבד)
+    def contact_us_fill_hotel(self, name, email, phone):
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel,choose_subject_dropdown, choose_subject, contact_note, contact_button = self.contact_locators()
+        contact_name.send_keys(name)
+        contact_email.send_keys(email)
+        contact_phone.send_keys(phone)
+
+        try:
+            choose_hotel_dropdown.click()
+            choose_hotel.click()
+        except Exception as e:
+            print("No hotel selected:")
+
+        try:
+            contact_button.click()
+        except Exception as e:
+            print("No contact:")
+
+    # פונקציה למילוי פרטים (עם בחירת נושא בלבד)
+    def contact_us_fill_subject(self, name, email, phone):
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel,choose_subject_dropdown, choose_subject, contact_note, contact_button = self.contact_locators()
+        contact_name.send_keys(name)
+        contact_email.send_keys(email)
+        contact_phone.send_keys(phone)
+
+        try:
+            choose_subject_dropdown.click()
+            choose_subject.click()
+        except Exception as e:
+            print("No subject selected:")
+
+        try:
+            contact_button.click()
+        except Exception as e:
+            print("No contact:")
