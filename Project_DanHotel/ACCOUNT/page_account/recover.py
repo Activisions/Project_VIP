6a1b61d66f5a0416657611e8119ab7eaca7bc2ab
ofir <ofir@lupa.co.il>
2024-09-01 20:16:13 +0300

    New pull R

--- Project_DanHotel/ACCOUNT/page_account/recover.py
+++ Project_DanHotel/ACCOUNT/page_account/recover.py
@@ -2,3 +2,34 @@ from imports import *
 
 
 
+class Recover:
+    def __init__(self, driver):
+        self.driver: WebDriver = driver
+
+    def recover_locators(self):
+        insert_mail = self.driver.find_element(By.ID,"mui-1")
+        by_sms = self.driver.find_element(By.XPATH,"(//*[@id='progress'])[1]")
+        by_mail = self.driver.find_element(By.XPATH,"(//*[@id='progress'])[2]")
+        return insert_mail , by_sms , by_mail
+
+
+    def recover_fill1(self,email):
+        insert_mail, by_sms, by_mail = self.recover_locators()
+        insert_mail.send_keys(email)
+        by_sms.click()
+
+    def recover_fill2(self, email):
+        insert_mail, by_sms, by_mail = self.recover_locators()
+        insert_mail.send_keys(email)
+        by_mail.click()
+
+
+    def recover_msg_validation_email(self):
+        msg_validation_email = self.driver.find_element(By.XPATH,"//*[@id='mui-1-helper-text']").text
+        return msg_validation_email
+
+
+    def recover_valid(self):
+        # recover_class = self.driver.find_element(By.XPATH,"//*[@class='phone disabled u1st-tabbable-element']")
+        recover_class = self.driver.find_element(By.ID,"progressWrapper")
+        return recover_class
--- Project_DanHotel/ACCOUNT/test_account/test_recover.py
+++ Project_DanHotel/ACCOUNT/test_account/test_recover.py
@@ -1,1 +1,52 @@
+import time
+
 from imports import *
+from Project_DanHotel.ACCOUNT.page_account.recover import *
+
+recover_url = "https://www.danhotels.co.il/eDan/Login"
+
+
+@pytest.fixture()
+def recovers(driver):
+    return Recover(driver)
+
+
+# בדיקה ששליחת שחזור חשבון עם מייל תקין עוברת
+def test_recover1(driver, recovers):
+    driver.get(recover_url)
+    recovers.recover_fill1("lupatest1@gmail.com")
+    actual_mail = recovers.recover_valid()
+    recover_msg = actual_mail.get_attribute('class')
+    assert recover_msg == "phone disabled"
+
+
+# בדיקה שקיים ולידציה למייל לא תקין דרך כפתור שחזור בSMS
+def test_recover2(driver, recovers):
+    driver.get(recover_url)
+    recovers.recover_fill1("test@@gmail.com")
+    actual_text = recovers.recover_msg_validation_email()
+    assert actual_text == 'אנא בדוק תקינות כתובת אימייל'
+
+
+# בדיקה שקיים ולידציה שאין כתובת מייל דרך כפתור שחזור בSMS
+def test_recover3(driver, recovers):
+    driver.get(recover_url)
+    recovers.recover_fill1("")
+    actual_text = recovers.recover_msg_validation_email()
+    assert actual_text == "אנא הכניסו כתובת אימייל"
+
+
+# בדיקה שקיים ולידציה למייל לא תקין דרך כפתור שחזור במייל
+def test_recover4(driver, recovers):
+    driver.get(recover_url)
+    recovers.recover_fill2("test@@gmail.com")
+    actual_text = recovers.recover_msg_validation_email()
+    assert actual_text == 'אנא בדוק תקינות כתובת אימייל'
+
+
+# בדיקה שקיים ולידציה שאין כתובת מייל דרך כפתור שחזור במייל
+def test_recover5(driver, recovers):
+    driver.get(recover_url)
+    recovers.recover_fill2("")
+    actual_text = recovers.recover_msg_validation_email()
+    assert actual_text == "אנא הכניסו כתובת אימייל"
