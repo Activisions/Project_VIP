22c996d6256bcecbd3bcb30a502bbea2f6783dca
ofir <ofir@lupa.co.il>
2024-09-01 17:25:55 +0300

    New pull R

--- Project_DanHotel/ACCOUNT/page_account/login.py
+++ Project_DanHotel/ACCOUNT/page_account/login.py
@@ -1,19 +1,16 @@
 from imports import *
 
 
-
 class Login:
     def __init__(self, driver):
         self.driver: WebDriver = driver
 
-
     def login_locators(self):
-         mailogin_button = self.driver.find_element(By.XPATH, "//*[@class='MuiButtonBase-root MuiTab-root MuiTab-textColorPrimary css-91lsx7']").click()
-         login_mail = self.driver.find_element(By.ID,"mui-2")
-         login_password = self.driver.find_element(By.ID,"mui-3")
-         login_button = self.driver.find_element(By.ID,"progress")
-         return login_mail, login_password, login_button
-
+        mailogin_button = self.driver.find_element(By.XPATH,"//*[@class='MuiButtonBase-root MuiTab-root MuiTab-textColorPrimary css-91lsx7']").click()
+        login_mail = self.driver.find_element(By.ID, "mui-2")
+        login_password = self.driver.find_element(By.ID, "mui-3")
+        login_button = self.driver.find_element(By.ID, "progress")
+        return login_mail, login_password, login_button
 
     def login_connect(self, email="lupatest1@gmail.com", password="a123123a"):
         login_mail, login_password, login_button = self.login_locators()
@@ -21,16 +18,33 @@ class Login:
         login_password.send_keys(password)
         login_button.click()
 
+
+
+
     def error_connect_valid(self):
         try:
-            loginmsg = self.driver.find_element(By.XPATH,"//*[@class='primary-controls']").text == "מייל או סיסמא שגויים"
+            loginmsg = self.driver.find_element(By.XPATH,
+                                                "//*[@class='primary-controls']").text == "מייל או סיסמא שגויים"
         except Exception as e:
             return None
-
 
     def error_connect_validmail(self):
         try:
             loginmsg2 = self.driver.find_element(By.XPATH, "//*[@id='mui-2-helper-text']").text
             return loginmsg2
+        except Exception as e:
+            return None
+
+    def error_connect_lessMail(self):
+        try:
+            loginmsg3 = self.driver.find_element(By.XPATH, "//*[@id='mui-2-helper-text']").text
+            return loginmsg3
+        except Exception as e:
+            return None
+
+    def error_connect_lessPass(self):
+        try:
+            loginmsg4 = self.driver.find_element(By.XPATH, "//*[@id='mui-3-helper-text']").text
+            return loginmsg4
         except Exception as e:
             return None
--- Project_DanHotel/ACCOUNT/test_account/test_login.py
+++ Project_DanHotel/ACCOUNT/test_account/test_login.py
@@ -1,32 +1,40 @@
-import time
-
 from imports import *
 from Project_DanHotel.ACCOUNT.page_account.login import *
 
-
-
-
 login_url = "https://www.danhotels.co.il/eDan/Login"
 
-#פונקציית טסט שמבצעת לוג אין בהצלחה
+
 @pytest.fixture()
 def connector(driver):
     return Login(driver)
 
-
-def test_login(driver,connector):
+#פונקציה שמבצעת חיבור בהצלחה
+def test_login(driver, connector):
     driver.get(login_url)
     connector.login_connect()
     assert connector.error_connect_valid() is None
     print("Login succeeded")
 
-
-
-def test_login2(driver,connector):
+#פונקציה שבודקת שלא ניתן להתחבר עם פרטים שגויים
+def test_login2(driver, connector):
     driver.get(login_url)
-    connector.login_connect("WrongMail.com","wrong")
+    connector.login_connect("WrongMail.com", "wrong")
     assert connector.error_connect_validmail() == 'אנא בדוק תקינות דוא"ל'
     print("Not login: Detail Validation succeeded")
+
+#פונקציה שבודקת ולידציה כאשר אין אימייל
+def test_login3(driver, connector):
+    driver.get(login_url)
+    connector.login_connect("", "Wrong@Mail.com")
+    assert connector.error_connect_lessMail() == 'אנא הכניסו דוא"ל'
+    print("Not login: Less Email Validation succeeded")
+
+#פונקציה שבודקת ולידציה כאשר אין סיסמא
+def test_login4(driver, connector):
+    driver.get(login_url)
+    connector.login_connect("lupatest1@gmail.com", "")
+    assert connector.error_connect_lessPass() == 'אנא הכניסו סיסמה'
+    print("Not login: Less Password Validation succeeded")
 
 
 
