ec25f4587fe89eb4e7e25b6c855965e36abbff65
ofir <ofir@lupa.co.il>
2024-09-04 22:24:41 +0300

    New pull R

--- Project_DanHotel/A_SITE/page_site/rooms.py
+++ Project_DanHotel/A_SITE/page_site/rooms.py
@@ -3,11 +3,9 @@ import time
 from imports import *
 
 
-
 class Rooms:
     def __init__(self, driver):
         self.driver: WebDriver = driver
-
 
     def element_find_hotel_menu_button(self):
         listHotel = self.driver.find_element(By.ID, "selectHotel")
@@ -17,44 +15,63 @@ class Rooms:
         selecthotel = self.driver.find_elements(By.XPATH,"//ul[@class='MuiList-root MuiList-padding MuiMenu-list css-r8u8y9']//li")
         return selecthotel
 
-
     def element_choose_date(self):
-        # listdate1 = self.driver.find_elements(By.XPATH, "//*[2]/table/tbody/tr[4]/td[5]/button/div")
-        # listdate1 = self.driver.find_elements(By.XPATH,"//*[@class='CalendarDay__button']")
         listdate1 = self.driver.find_elements(By.XPATH,"//*[2]/table/tbody/tr/td/*[@class='CalendarDay__button']/div[1]")
-        listdate2 = self.driver.find_elements(By.XPATH,"//*[2]/table/tbody/tr/td/*[@class='CalendarDay__button']")
-        nextdate = self.driver.find_element(By.XPATH,"//*[@class='DayPickerNavigation__next DayPickerNavigation__next--rtl']")
-        return (listdate1,listdate2,nextdate)
+        listdate2 = self.driver.find_elements(By.XPATH, "//*[2]/table/tbody/tr/td/*[@class='CalendarDay__button']")
+        nextmonth = self.driver.find_element(By.XPATH,"//*[@class='DayPickerNavigation__next DayPickerNavigation__next--rtl']")
+        return (listdate1, listdate2, nextmonth)
+
+
+    AddAnotherRoomButton = (By.CLASS_NAME, "addAnother")
+    AddRoomMenu = (By.XPATH,"//div[@class='MuiSelect-select MuiSelect-outlined MuiOutlinedInput-input MuiInputBase-input css-qiwgdb']")
+    AddRoomClick = (By.XPATH, "//li[3]/span")
+    CheckPriceButton = (By.CLASS_NAME, "updateRates")
+
+    Errorvalidation_CheckPriceButton = (By.XPATH, "//*[@class='small-modal inner_content_text']/div[@class='innerBody']")
+    error_hotel_valid = (By.CLASS_NAME, "errors errHotel")
+    error_checkin_valid = (By.CLASS_NAME, "errors"[1])
+    error_checkout_valid = (By.CLASS_NAME, "errors"[2])
 
 
 
 
-################################################################
+    #################################testlist###############################
 
 
-    def select_hotel_menu(self):
+    def select_hotel_menu(self,hotel):
         self.element_find_hotel_menu_button()
         selecthotel = self.element_find_hotel_menu_list()
-        selecthotel[21].click()
+        hotelname = selecthotel[hotel].text
+        selecthotel[hotel].click()
         time.sleep(1)
+        return hotelname
 
+    def select_date_menu(self, CheckIN, CheckOUT):
+        try:
+            while True:
+                time.sleep(1)
+                listdate1, listdate2, nextmonth = self.element_choose_date()
+                outdate = "לא זמין"
+                checkdate = listdate2[CheckIN - 1].get_attribute("aria-label")
+                checkdate2 = listdate2[CheckOUT - 1].get_attribute("aria-label")
+                if checkdate.startswith(outdate) and checkdate2.startswith(outdate):
+                    nextmonth.click()
+                else:
+                    break
+            print(checkdate, checkdate2)
+            listdate1[CheckIN - 1].click()
+            listdate1[CheckOUT - 1].click()
+        except: return "הוכנס תאריך באופן שגוי"
 
-    def select_date_menu(self,date1=20,date2=22):
-        listdate1,listdate2,nextdate = self.element_choose_date()
-        checkdate = listdate2[date1-1].get_attribute("aria-label")
-        checkdate2 = listdate2[date2-1].get_attribute("aria-label")
-        print(checkdate,checkdate2)
-        if checkdate.startswith("לא זמין") and checkdate2.startswith("לא זמין"):
-            nextdate.click()
-        else:
-            listdate1[date1-1].click()
-            listdate1[date2-1].click()
-
-
-
-
-
-
-
-
-
+    def AddAnotherRoom(self):
+        self.driver.find_element(*self.AddAnotherRoomButton).click()
+        for x in range(0, 7):
+            if x == 0:
+                continue
+            ADDROOM = self.driver.find_elements(*self.AddRoomMenu)
+            ADDROOM[x].click()
+            self.driver.find_element(*self.AddRoomClick).click()
+        self.driver.find_element(*self.CheckPriceButton).click()
+        time.sleep(1)
+        textvalid = self.driver.find_element(*self.Errorvalidation_CheckPriceButton)
+        return textvalid
--- Project_DanHotel/A_SITE/test_site/test_rooms.py
+++ Project_DanHotel/A_SITE/test_site/test_rooms.py
@@ -1,14 +1,49 @@
 from Project_DanHotel.A_SITE.page_site.rooms import Rooms
 from imports import *
 
-
-
 rooms_url = "https://www.danhotels.co.il/Booking/SearchResults?fr=htl&com=box&ttl=ChooseYourRoom&Lang=heb&editMode=true"
 
 
-def test_test(driver):
+# בודק שהמלון שוהכנס תקין , וניתן לבחור תאריכי כניסה ויציאה
+def test_select_hotel_menu(driver):
     driver.get(rooms_url)
     room = Rooms(driver)
     room.select_hotel_menu()
-    room.select_date_menu()
+    room.select_date_menu(20, 25)
+    assert room.select_hotel_menu() == "דן פנורמה תל אביב"
 
+
+#   טסט שבוחר מלון שהתאריכים בו לא זמינים ומתקדם כל פעם חודש קדימה ובודק אם פנוי
+def test_full_booking_hotel(driver):
+    driver.get(rooms_url)
+    room = Rooms(driver)
+    room.select_hotel_menu(21)
+    room.select_date_menu(20, 25)
+    assert room.select_hotel_menu(21) == "רות צפת"
+
+
+# פותח אופציה לחדרים נוספים וממלא את כל התפריטים - בודק שיש ולידציה פופ אפ לאחר התקדמות
+def test_hotel_with_more_rooms(driver):
+    driver.get(rooms_url)
+    room = Rooms(driver)
+    room.select_hotel_menu()
+    room.select_date_menu(20, 25)
+    assert room.AddAnotherRoom().text == "אורח יקר, התפוסה המבוקשת בחדר הראשון שבחרת אינה מתאימה לשהייה בחדר אחד. אנא פצלו את ההרכב ובצעו חיפוש מחדש."
+
+
+# בודק שלא ניתן להכניס תאריך שגוי
+def test_wrong_date_menu(driver):
+    driver.get(rooms_url)
+    room = Rooms(driver)
+    room.select_hotel_menu()
+    result = room.select_date_menu(30, 40)
+    assert result == "הוכנס תאריך באופן שגוי"
+
+
+# בודק שלא ניתן להכניס תאריך 31 כאשר אין בחודש הנוכחי תאריך כזה
+def test_date31_menu(driver):
+    driver.get(rooms_url)
+    room = Rooms(driver)
+    room.select_hotel_menu()
+    result = room.select_date_menu(30, 31)
+    assert result == "הוכנס תאריך באופן שגוי"
