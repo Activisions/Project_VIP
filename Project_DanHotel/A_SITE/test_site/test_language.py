7e3e4ab9a22cb8d3b6f56cabe8510068e221ebce
galsh402 <134851412+galsh402@users.noreply.github.com>
2024-09-07 22:00:57 +0300

    New pull R

--- Project_DanHotel/A_SITE/test_site/test_language.py
+++ Project_DanHotel/A_SITE/test_site/test_language.py
@@ -2,7 +2,7 @@ from imports import *
 from Project_DanHotel.A_SITE.page_site.language import *
 
 hebrew_url = "https://www.danhotels.co.il"
-english_url = "https://www.danhotels.com/"
+english_url = "https://www.danhotels.com/?_gl=1"
 
 
 def test_change_language_to_english(driver):
@@ -15,13 +15,14 @@ def test_change_language_to_english_Check_url(driver):
     driver.get(hebrew_url)
     switch_to_english = language(driver)
     switch_to_english.url_change()
+    assert switch_to_english.url_change() == english_url
 
 
 def test_change_language_to_english_check_element(driver):
     driver.get(hebrew_url)
     switch_to_english = language(driver)
-    switch_to_english.check_element()
-
+    switch_to_english.english_site_mood()
+    assert switch_to_english.check_element() == "Dan Hotels"
 
 hebrew_url = "https://www.danhotels.co.il"
 
