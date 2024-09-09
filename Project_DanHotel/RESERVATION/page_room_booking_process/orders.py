190a0139c7d994be7d0f12207f5bae0fc63a451a
ofir <ofir@lupa.co.il>
2024-09-09 22:58:13 +0300

    New pull R

--- Project_DanHotel/RESERVATION/page_room_booking_process/orders.py
+++ Project_DanHotel/RESERVATION/page_room_booking_process/orders.py
@@ -19,19 +19,36 @@ class Orders:
     #         print(price[matches].text)
 
 
+    # def match_price(self):
+    #     price = self.driver.find_elements(*self.price)
+    #     club_price = self.driver.find_elements(*self.club_price)
+    #     checker = True
+    #     for matches in price:
+    #         price = matches.text
+    #         for matches2 in club_price:
+    #             club_price = matches2.text
+    #             if price < club_price:
+    #                 checker = False
+    #                 print("מחיר מועדון גדול ממחיר אתר")
+    #             else:
+    #                 print("הבדיקה עברה בהצלחה")
+    #     return checker
+
     def match_price(self):
-        price = self.driver.find_elements(*self.price)
-        club_price = self.driver.find_elements(*self.club_price)
-        for matches in price:
-            price = matches.text
-            for matches2 in club_price:
-                club_price = matches2.text
-                if price < club_price:
-                    print("מחיר מועדון גדול ממחיר אתר")
-                else:
-                    print("הבדיקה עברה בהצלחה")
+        prices = self.driver.find_elements(*self.price)
+        club_prices = self.driver.find_elements(*self.club_price)
+        checker = True
+        for price_element in prices:
+            price_value = int(price_element.text.replace('₪', '').replace(',', '').strip())
+            for club_price_element in club_prices:
+                club_price_value = int(club_price_element.text.replace('₪', '').replace(',', '').strip())
+                if club_price_value > price_value:
+                    checker = False
+                    print(f"price {price_value} club {club_price_value}")
+                    return checker
 
-
+        print("הבדיקה עברה בהצלחה")
+        return checker
 
 
 def test_match_price(driver):
