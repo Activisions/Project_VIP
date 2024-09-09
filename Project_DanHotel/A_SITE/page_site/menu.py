7715685d587cd6c1366f53383afd3d43e12f5674
ofir <ofir@lupa.co.il>
2024-09-09 17:38:55 +0300

    New pull R

--- Project_DanHotel/A_SITE/page_site/menu.py
+++ Project_DanHotel/A_SITE/page_site/menu.py
@@ -1,3 +1,5 @@
+import time
+
 from imports import *
 
 
@@ -10,26 +12,6 @@ class Menu:
 
     def get_menu_and_page_text(self):
         menu_items = self.driver.find_elements(*self.menu_links)
-        result = []
-
-        for i in range(len(menu_items)):
-            # Refetch the menu items in each iteration to avoid stale element references
+        for open in range (len(menu_items)):
             menu_items = self.driver.find_elements(*self.menu_links)
-            item = menu_items[i]
-
-            # Click each menu item
-            item.click()
-
-            # Wait for the page text to load
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located(self.page_text)
-            )
-
-            # Get the text from the page and menu item
-            page_text_content = self.driver.find_element(*self.page_text).text
-            menu_item_text = item.text
-
-            # Store the menu item text and page text for pytest assertions
-            result.append((menu_item_text, page_text_content))
-
-        return result
+            menu_items[open].click()
--- Project_DanHotel/A_SITE/test_site/test_menu.py
+++ Project_DanHotel/A_SITE/test_site/test_menu.py
@@ -1,12 +1,10 @@
 from imports import *
 from Project_DanHotel.A_SITE.page_site.menu import Menu
 
-urllinks = "https://www.danhotels.co.il/AboutDanhotels/Contactus"
+urllinks = "https://www.danhotels.co.il/AboutDanhotels/Overview"
 
 
 def test_menu(driver):
     driver.get(urllinks)
     menu = Menu(driver)
-    menu_texts = menu.get_menu_and_page_text()
-    for menu_item_text, page_text_content in menu_texts:
-        assert menu_item_text in page_text_content, f"Menu item {menu_item_text} not found in page text"
+    menu.get_menu_and_page_text()
