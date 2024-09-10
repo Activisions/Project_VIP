9becc1711371093a95441526565280bbc0467db7
ofir <ofir@lupa.co.il>
2024-09-10 11:46:32 +0300

    done orders

--- Project_DanHotel/A_SITE/page_site/orders.py
+++ Project_DanHotel/A_SITE/page_site/orders.py
@@ -26,13 +26,6 @@ class Orders:
         return checker
 
 
-def test_match_price(driver):
-    driver.get("https://www.danhotels.co.il/Booking/SearchResults?fr=hp&com=box&ttl=ChooseYourRoom&SelectedHotelID=10118&ListHotelIds=10118&CheckIn=19.09.2024&CheckOut=20.09.2024&Pax[0].Adults=2&Pax[0].Children=0&Pax[0].Infants=0&site=dan")
-    match = Orders(driver)
-    time.sleep(9)
-    result = match.match_price()
-    assert result == True
-
 
 
 
