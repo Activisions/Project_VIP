420c88524682113e649d064c4cec8183f71ebf00
ofir <ofir@lupa.co.il>
2024-09-10 14:12:29 +0300

    done orders

--- Project_DanHotel/A_SITE/page_site/menu.py
+++ Project_DanHotel/A_SITE/page_site/menu.py
@@ -8,13 +8,20 @@ class Menu:
         self.driver = driver
 
     menu_links = (By.XPATH, "//nav[@class='about-menu he']//ul//li/a")
+    tagame = (By.XPATH,"//article/div/div/div[1]/div/div/span")
 
 
     def get_menu_and_page_text(self):
         menu_items = self.driver.find_elements(*self.menu_links)
         for runner in range(len(menu_items)):
-            if runner == 10:
+            if runner == 11:
                 continue
             menu_items = self.driver.find_elements(*self.menu_links)
+            compare = menu_items[runner].text
             menu_items[runner].click()
+            tagame = self.driver.find_element(*self.tagame)
+            tagame_text = tagame.text
+            if compare not in tagame_text:
+                print(f"Need fix menu title: {tagame_text}")
+        return len(menu_items)
 
--- Project_DanHotel/A_SITE/test_site/test_menu.py
+++ Project_DanHotel/A_SITE/test_site/test_menu.py
@@ -1,10 +1,21 @@
 from imports import *
 from Project_DanHotel.A_SITE.page_site.menu import Menu
 
-urllinks = "https://www.danhotels.co.il/AboutDanhotels/Overview"
+about_buttons = "https://www.danhotels.co.il/AboutDanhotels/Overview"
+accessibility_buttons = "https://www.danhotels.co.il/AccessibilityStatement"
 
 
+
+# בדיקה שעוברת על כל הפתורים בתפריט יצירת קשר
 def test_menu(driver):
-    driver.get(urllinks)
+    driver.get(about_buttons)
     menu = Menu(driver)
-    menu.get_menu_and_page_text()
+    menubutton = menu.get_menu_and_page_text()
+    assert menubutton == 17
+
+# בדיקה שעוברת על כל הכפתורים בתפריט הסדרי נגישות
+def test_menu2(driver):
+    driver.get(accessibility_buttons)
+    menu = Menu(driver)
+    menubutton = menu.get_menu_and_page_text()
+    assert menubutton == 18
