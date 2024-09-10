d03ee3fd28b283ae148dc3800db34bea67fa1793
galsh402 <134851412+galsh402@users.noreply.github.com>
2024-09-10 21:12:19 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Project_DanHotel/A_SITE/page_site/language.py
    #	Project_DanHotel/A_SITE/test_site/test_language.py

--- Project_DanHotel/A_SITE/page_site/contact_us.py
+++ Project_DanHotel/A_SITE/page_site/contact_us.py
@@ -15,12 +15,14 @@ class contact_Us:
         choose_subject = self.driver.find_element(By.XPATH, "//*[@id=\"edit-subject\"]/option[5]")
         contact_note = self.driver.find_element(By.ID, "edit-message")
         contact_button = self.driver.find_element(By.NAME, "op")
+        H1_title =self.driver.find_element(By.XPATH,"//*[@id=\"block-danhotel-content\"]/div/div[3]/div/div[2]/article/div/div/div[1]/div/div/span")
+        error_wrong_name =self.driver.find_element(By.ID, "edit-name-error")
         self.driver.execute_script("arguments[0].scrollIntoView();",contact_button)
-        return contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button
+        return contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button, H1_title, error_wrong_name
 
     #פונקציה למילוי פרטים (טלפון ,אימייל ,טלפון)
     def contact_us_fill(self, name, email, phone, nots):
-        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button = self.contact_locators()
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button, H1_title, error_wrong_name = self.contact_locators()
         contact_name.send_keys(name)
         contact_email.send_keys(email)
         contact_phone.send_keys(phone)
@@ -29,7 +31,7 @@ class contact_Us:
 
     #פונקציה למילוי פרטים (עם בחירת מלון בלבד)
     def contact_us_fill_hotel(self, name, email, phone):
-        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button = self.contact_locators()
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button, H1_title, error_wrong_name = self.contact_locators()
         contact_name.send_keys(name)
         contact_email.send_keys(email)
         contact_phone.send_keys(phone)
@@ -39,10 +41,23 @@ class contact_Us:
 
     #פונקציה למילוי פרטים (עם בחירת נושא בלבד)
     def contact_us_fill_subject(self, name, email, phone):
-        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button = self.contact_locators()
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button, H1_title, error_wrong_name = self.contact_locators()
         contact_name.send_keys(name)
         contact_email.send_keys(email)
         contact_phone.send_keys(phone)
         choose_subject_dropdown.click()
         choose_subject.click()
         # contact_button.click()
+
+    def contact_us_fill_text(self):
+        contact_name, contact_email, contact_phone, choose_hotel_dropdown, choose_hotel, choose_subject_dropdown, choose_subject, contact_note, contact_button, H1_title, error_wrong_name = self.contact_locators()
+        check_text = H1_title.text
+        check_error1 = error_wrong_name.text
+        return check_text, check_error1
+
+
+    #def contact_us_fill_hotel_text(self):
+
+
+    #def contact_us_fill_subject_text(self):
+
--- Project_DanHotel/A_SITE/test_site/test_contact_us.py
+++ Project_DanHotel/A_SITE/test_site/test_contact_us.py
@@ -7,6 +7,7 @@ def test_Contactus1(driver):
     driver.get(Contactus_url)
     contact_filler = contact_Us(driver)
     contact_filler.contact_us_fill("גל", "test@gmail.com", "0530000000", "test")
+    assert contact_filler.contact_us_fill_text() == "צור קשר"
     print("contact succeeded")
 
 
@@ -14,8 +15,7 @@ def test_Contactus2(driver):
     driver.get(Contactus_url)
     contact_filler = contact_Us(driver)
     contact_filler.contact_us_fill("ג", "test@gmail.com", "0530000000","test")
-    error_wrong_name = driver.find_element(By.ID, "edit-name-error")
-    assert error_wrong_name.text == "Please enter at least 2 characters."
+    assert contact_filler.contact_us_fill_text() == "Please enter at least 2 characters."
     print("Name validation succeeded")
 
 
@@ -23,7 +23,7 @@ def test_Contactus3(driver):
     driver.get(Contactus_url)
     contact_filler = contact_Us(driver)
     contact_filler.contact_us_fill("גל", "@.com", "0530000000","test")
-    error_wrong_name = driver.find_element(By.ID, "edit-email-error")
+    #error_wrong_name = driver.find_element(By.ID, "edit-email-error")
     assert error_wrong_name.text == "אמייל does not contain a valid email."
     print("e-mail validation succeeded")
 
@@ -44,7 +44,7 @@ def test_Contactus5(driver):
 
 def test_Contactus6(driver):
     driver.get(Contactus_url)
-    operating_Hours = driver.find_element(By.XPATH, "//*[@id=\"block-danhotel-content\"]/div/div[3]/div/div[2]/article/div/div/div[3]/div/div[2]/div/p[1]")
+    #operating_Hours = driver.find_element(By.XPATH, "//*[@id=\"block-danhotel-content\"]/div/div[3]/div/div[2]/article/div/div/div[3]/div/div[2]/div/p[1]")
     operating_Hours = operating_Hours.text
     assert operating_Hours == "ראשון-חמישי: 8:00 - 19:00"
     print(operating_Hours)
